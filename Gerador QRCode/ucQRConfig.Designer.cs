f9d4dc73d23dd3c9209178f4c41b9c65d573c6cf
Gustavo Barcario <gustavobarcario.0788@gmail.com>
2022-12-04 18:08:57 -0300

    Cria menu UserControl

---
+++ Gerador QRCode/ucQRConfig.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Gerador_QRCode
+{
+    partial class ucQRConfig
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTamanho = new System.Windows.Forms.ComboBox();
+            this.cmbCharsetSource = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbEcc = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbCor = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbFormato = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbQzone = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbMargem = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbCorFundo = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(525, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Configurações";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tamanho:";
+            // 
+            // cmbTamanho
+            // 
+            this.cmbTamanho.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTamanho.FormattingEnabled = true;
+            this.cmbTamanho.Location = new System.Drawing.Point(8, 118);
+            this.cmbTamanho.Name = "cmbTamanho";
+            this.cmbTamanho.Size = new System.Drawing.Size(240, 24);
+            this.cmbTamanho.TabIndex = 2;
+            // 
+            // cmbCharsetSource
+            // 
+            this.cmbCharsetSource.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCharsetSource.FormattingEnabled = true;
+            this.cmbCharsetSource.Location = new System.Drawing.Point(8, 184);
+            this.cmbCharsetSource.Name = "cmbCharsetSource";
+            this.cmbCharsetSource.Size = new System.Drawing.Size(240, 24);
+            this.cmbCharsetSource.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Charset Source:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(8, 250);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(240, 24);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Charset Target:";
+            // 
+            // cmbEcc
+            // 
+            this.cmbEcc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEcc.FormattingEnabled = true;
+            this.cmbEcc.Location = new System.Drawing.Point(8, 316);
+            this.cmbEcc.Name = "cmbEcc";
+            this.cmbEcc.Size = new System.Drawing.Size(240, 24);
+            this.cmbEcc.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "ECC:";
+            // 
+            // cmbCor
+            // 
+            this.cmbCor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCor.FormattingEnabled = true;
+            this.cmbCor.Location = new System.Drawing.Point(8, 382);
+            this.cmbCor.Name = "cmbCor";
+            this.cmbCor.Size = new System.Drawing.Size(240, 24);
+            this.cmbCor.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 353);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Cor:";
+            // 
+            // cmbFormato
+            // 
+            this.cmbFormato.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFormato.FormattingEnabled = true;
+            this.cmbFormato.Location = new System.Drawing.Point(272, 316);
+            this.cmbFormato.Name = "cmbFormato";
+            this.cmbFormato.Size = new System.Drawing.Size(240, 24);
+            this.cmbFormato.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(267, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 25);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Formato:";
+            // 
+            // cmbQzone
+            // 
+            this.cmbQzone.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbQzone.FormattingEnabled = true;
+            this.cmbQzone.Location = new System.Drawing.Point(272, 250);
+            this.cmbQzone.Name = "cmbQzone";
+            this.cmbQzone.Size = new System.Drawing.Size(240, 24);
+            this.cmbQzone.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(267, 221);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Qzone:";
+            // 
+            // cmbMargem
+            // 
+            this.cmbMargem.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMargem.FormattingEnabled = true;
+            this.cmbMargem.Location = new System.Drawing.Point(272, 184);
+            this.cmbMargem.Name = "cmbMargem";
+            this.cmbMargem.Size = new System.Drawing.Size(240, 24);
+            this.cmbMargem.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(267, 155);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 25);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Margem:";
+            // 
+            // cmbCorFundo
+            // 
+            this.cmbCorFundo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCorFundo.FormattingEnabled = true;
+            this.cmbCorFundo.Location = new System.Drawing.Point(272, 118);
+            this.cmbCorFundo.Name = "cmbCorFundo";
+            this.cmbCorFundo.Size = new System.Drawing.Size(240, 24);
+            this.cmbCorFundo.TabIndex = 12;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(267, 89);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(151, 25);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Cor de Fundo:";
+            // 
+            // ucQRConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cmbFormato);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbQzone);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmbMargem);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cmbCorFundo);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cmbCor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbEcc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbCharsetSource);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbTamanho);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ucQRConfig";
+            this.Size = new System.Drawing.Size(525, 424);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTamanho;
+        private System.Windows.Forms.ComboBox cmbCharsetSource;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbEcc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbCor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbFormato;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbQzone;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cmbMargem;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbCorFundo;
+        private System.Windows.Forms.Label label10;
+    }
+}
