856a59b7f441eabaf5398fa7cbc6658a70965f11
Gustavo Barcario <gustavobarcario.0788@gmail.com>
2022-12-04 17:12:56 -0300

    Cria menu lateral

---
+++ Gerador QRCode/FrmMDI.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Gerador_QRCode
+{
+    partial class FrmMDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMDI));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnWifiType = new System.Windows.Forms.Button();
+            this.btnEmailType = new System.Windows.Forms.Button();
+            this.btnEventoType = new System.Windows.Forms.Button();
+            this.btnLocalizacaoType = new System.Windows.Forms.Button();
+            this.btnLigarType = new System.Windows.Forms.Button();
+            this.btnSmsType = new System.Windows.Forms.Button();
+            this.btnContatoType = new System.Windows.Forms.Button();
+            this.btnTextoType = new System.Windows.Forms.Button();
+            this.btnUrlType = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 661);
+            this.panel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnWifiType, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.btnEmailType, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.btnEventoType, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnLocalizacaoType, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btnLigarType, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnSmsType, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnContatoType, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnTextoType, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnUrlType, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(250, 661);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnWifiType
+            // 
+            this.btnWifiType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnWifiType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWifiType.Image = global::Gerador_QRCode.Properties.Resources.wifi;
+            this.btnWifiType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnWifiType.Location = new System.Drawing.Point(3, 587);
+            this.btnWifiType.Name = "btnWifiType";
+            this.btnWifiType.Size = new System.Drawing.Size(244, 71);
+            this.btnWifiType.TabIndex = 8;
+            this.btnWifiType.Text = "Wi-fi";
+            this.btnWifiType.UseVisualStyleBackColor = true;
+            // 
+            // btnEmailType
+            // 
+            this.btnEmailType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnEmailType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmailType.Image = global::Gerador_QRCode.Properties.Resources.email;
+            this.btnEmailType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmailType.Location = new System.Drawing.Point(3, 514);
+            this.btnEmailType.Name = "btnEmailType";
+            this.btnEmailType.Size = new System.Drawing.Size(244, 67);
+            this.btnEmailType.TabIndex = 7;
+            this.btnEmailType.Text = "E-mail";
+            this.btnEmailType.UseVisualStyleBackColor = true;
+            // 
+            // btnEventoType
+            // 
+            this.btnEventoType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnEventoType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEventoType.Image = global::Gerador_QRCode.Properties.Resources._event;
+            this.btnEventoType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEventoType.Location = new System.Drawing.Point(3, 441);
+            this.btnEventoType.Name = "btnEventoType";
+            this.btnEventoType.Size = new System.Drawing.Size(244, 67);
+            this.btnEventoType.TabIndex = 6;
+            this.btnEventoType.Text = "Evento";
+            this.btnEventoType.UseVisualStyleBackColor = true;
+            // 
+            // btnLocalizacaoType
+            // 
+            this.btnLocalizacaoType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnLocalizacaoType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocalizacaoType.Image = global::Gerador_QRCode.Properties.Resources.geolocalization;
+            this.btnLocalizacaoType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLocalizacaoType.Location = new System.Drawing.Point(3, 368);
+            this.btnLocalizacaoType.Name = "btnLocalizacaoType";
+            this.btnLocalizacaoType.Size = new System.Drawing.Size(244, 67);
+            this.btnLocalizacaoType.TabIndex = 5;
+            this.btnLocalizacaoType.Text = "Localizador";
+            this.btnLocalizacaoType.UseVisualStyleBackColor = true;
+            // 
+            // btnLigarType
+            // 
+            this.btnLigarType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnLigarType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLigarType.Image = global::Gerador_QRCode.Properties.Resources.call;
+            this.btnLigarType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLigarType.Location = new System.Drawing.Point(3, 295);
+            this.btnLigarType.Name = "btnLigarType";
+            this.btnLigarType.Size = new System.Drawing.Size(244, 67);
+            this.btnLigarType.TabIndex = 4;
+            this.btnLigarType.Text = "Ligar";
+            this.btnLigarType.UseVisualStyleBackColor = true;
+            // 
+            // btnSmsType
+            // 
+            this.btnSmsType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSmsType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSmsType.Image = global::Gerador_QRCode.Properties.Resources.sms;
+            this.btnSmsType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSmsType.Location = new System.Drawing.Point(3, 222);
+            this.btnSmsType.Name = "btnSmsType";
+            this.btnSmsType.Size = new System.Drawing.Size(244, 67);
+            this.btnSmsType.TabIndex = 3;
+            this.btnSmsType.Text = "SMS";
+            this.btnSmsType.UseVisualStyleBackColor = true;
+            // 
+            // btnContatoType
+            // 
+            this.btnContatoType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnContatoType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContatoType.Image = global::Gerador_QRCode.Properties.Resources.vcard;
+            this.btnContatoType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnContatoType.Location = new System.Drawing.Point(3, 149);
+            this.btnContatoType.Name = "btnContatoType";
+            this.btnContatoType.Size = new System.Drawing.Size(244, 67);
+            this.btnContatoType.TabIndex = 2;
+            this.btnContatoType.Text = "Contato";
+            this.btnContatoType.UseVisualStyleBackColor = true;
+            // 
+            // btnTextoType
+            // 
+            this.btnTextoType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnTextoType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTextoType.Image = global::Gerador_QRCode.Properties.Resources.text;
+            this.btnTextoType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTextoType.Location = new System.Drawing.Point(3, 76);
+            this.btnTextoType.Name = "btnTextoType";
+            this.btnTextoType.Size = new System.Drawing.Size(244, 67);
+            this.btnTextoType.TabIndex = 1;
+            this.btnTextoType.Text = "Texto";
+            this.btnTextoType.UseVisualStyleBackColor = true;
+            // 
+            // btnUrlType
+            // 
+            this.btnUrlType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnUrlType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.765218F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUrlType.Image = global::Gerador_QRCode.Properties.Resources.url;
+            this.btnUrlType.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUrlType.Location = new System.Drawing.Point(3, 3);
+            this.btnUrlType.Name = "btnUrlType";
+            this.btnUrlType.Size = new System.Drawing.Size(244, 67);
+            this.btnUrlType.TabIndex = 0;
+            this.btnUrlType.Text = "URL";
+            this.btnUrlType.UseVisualStyleBackColor = true;
+            // 
+            // FrmMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1427, 663);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Name = "FrmMDI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QRCode Gerador";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnWifiType;
+        private System.Windows.Forms.Button btnEmailType;
+        private System.Windows.Forms.Button btnEventoType;
+        private System.Windows.Forms.Button btnLocalizacaoType;
+        private System.Windows.Forms.Button btnLigarType;
+        private System.Windows.Forms.Button btnSmsType;
+        private System.Windows.Forms.Button btnContatoType;
+        private System.Windows.Forms.Button btnTextoType;
+        private System.Windows.Forms.Button btnUrlType;
+    }
+}
+
